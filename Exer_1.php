3e49674b10d1163fadb7927cae47b96d0eea1d34
Richard <samira.pontes@cs.unipe.edu.br>
2025-08-22 07:51:34 -0300

    exemplo

---
+++ Exer_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo PHP</title>
+</head>
+<body>
+    <h1>Olá, mundo!</h1>
+
+    <?php
+        $nome = "Samira";
+        echo "<p>Bem-vinda, $nome!</p>";
+    ?>
+</body>
+</html>
