a380141392f4442a1f8692efbf2c4876e546a536
samira083 <samira.pontes@cs.unipe.edu.br>
2025-08-22 16:14:03 -0300

    salvar

--- Exer_1.php
+++ Exer_1.php
@@ -1,24 +1,24 @@
 <?php
-if(isset($_GET['valor'])) {
-    $x = $_GET['valor'];
+if (isset($_POST['custo'])) {
+    $custo = floatval($_POST['custo']); // pega o custo de fábrica
 
-    if ($x <= 39999.99) {
-        $soma_1 = 0.05 * $x;
-        $resultado = $soma_1 + $x;
-    } elseif($x > 40000 && $x <= 69999.99) {
-        $soma_2 = 0.10* $x;
-        $soma_3 = 0.15* $x;
-        $resultado = $soma_3 + $soma_2 + $x;
-    } elseif( $x > 69999.99){
-        $soma_4 = 0.15 * $x;
-        $soma_5 = 0.20 * $x;
-        $resultado = $soma_5 + $soma_4 + $x;
-    } else {
-        echo "defina um valor correto.";
-        exit;
+    // Define as porcentagens de distribuidor e impostos
+    if ($custo <= 39999.99) {
+        $percDistribuidor = 5;
+        $percImposto = 5;
+    } elseif ($custo <= 69999.99) {
+        $percDistribuidor = 10;
+        $percImposto = 15;
+    } else { // acima de 69999
+        $percDistribuidor = 15;
+        $percImposto = 20;
     }
 
-    echo"O valor final é R$" . number_format($resultado,0,",",".");
+    // Calcula preço final
+    $precoFinal = $custo + ($custo * $percDistribuidor / 100) + ($custo * $percImposto / 100);
+
+    echo "<h2>Preço ao consumidor: R$ " . number_format($precoFinal, 2, ',', '.') . "</h2>";
+} else {
+    echo "Digite um valor de custo de fábrica.";
 }
 ?>
-
