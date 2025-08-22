63ae576f5a3289193dc34f88bd0f98b5af0f0520
samira083 <samira.pontes@cs.unipe.edu.br>
2025-08-22 13:32:51 -0300

    codigo com erros

--- Exer_1.php
+++ Exer_1.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Exemplo PHP</title>
-</head>
-<body>
-    <h1>Olá, mundo!</h1>
+<?php
+if(isset($_GET['valor'])) {
+    $x = $_GET['valor'];
 
-    <?php
-        $nome = "Samira";
-        echo "<p>Bem-vinda, $nome!</p>";
-    ?>
-</body>
-</html>
+    if ($x <= 39999.99) {
+        $soma_1 = 0.05 * $x;
+        $resultado = $soma_1 + $x;
+    } elseif($x > 40000 && $x <= 69999.99) {
+        $soma_2 = 0.10* $x;
+        $soma_3 = 0.15* $x;
+        $resultado = $soma_3 + $soma_2 + $x;
+    } elseif( $x > 69999.99){
+        $soma_4 = 0.15 * $x;
+        $soma_5 = 0.20 * $x;
+        $resultado = $soma_5 + $soma_4 + $x;
+    } else {
+        echo "defina um valor correto.";
+        exit;
+    }
+
+    echo"O valor final é R$" . number_format($resultado,0,",",".");
+}
+?>
+
